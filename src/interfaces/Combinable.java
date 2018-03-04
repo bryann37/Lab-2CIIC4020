59f3705cd47d464129a78e022b858cae016f2f29
bryanbonilla <bryanbonilla@192.168.0.18>
2018-03-04 19:23:12 -0400

    Implemented the Combinable Interface

---
+++ src/interfaces/Combinable.java
@@ -0,0 +1,10 @@
+package interfaces;
+
+import orderedStructures.Arithmetic;
+import orderedStructures.Progression;
+
+public interface Combinable {
+	public Progression addto(Arithmetic c2);
+	public Progression substractFrom(Arithmetic c2);
+
+}
--- src/orderedStructures/Arithmetic.java
+++ src/orderedStructures/Arithmetic.java
@@ -1,6 +1,8 @@
 package orderedStructures;
 
-public class Arithmetic extends Progression {
+import interfaces.Combinable;
+
+public class Arithmetic extends Progression implements Combinable {
 	private double commonDifference; 
 	
 	public Arithmetic(double firstValue, double commonDifference) { 
@@ -30,6 +32,23 @@ public class Arithmetic extends Progression {
 		double value = (this.firstValue() + commonDifference * (n-1)); 
 		
 		return value; 
+	}
+
+	@Override
+	public Progression addto(Arithmetic c2) {
+		double newfirst = this.firstValue() + c2.firstValue();
+		double newdif = this.commonDifference + c2.commonDifference;
+		Progression newP = new Arithmetic(newfirst,newdif);
+		return newP;
+		
+	}
+
+	@Override
+	public Progression substractFrom(Arithmetic c2) {
+		double newfirst = this.firstValue() - c2.firstValue();
+		double newdif = this.commonDifference - c2.commonDifference;
+		Progression newP = new Arithmetic(newfirst,newdif);
+		return newP;
 	}
 	
 
